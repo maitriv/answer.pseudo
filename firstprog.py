6081a5fbbabcfdc8325f07260d2f1137d47eb8d9
maitriv <maitrivhegde@gmail.com>
2015-11-23 19:04:09 +0530

    required code is in python

---
+++ firstprog.py
@@ -0,0 +1,9 @@
+import random
+
+#this takes the name from the user 
+name=raw_input() 
+
+#this gives the required output with					
+
+#random numbers between 5 and 15 
+print(name+" "+str(random.randint(6,14)))
